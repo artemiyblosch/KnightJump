f037720acfd7d3e805914a2c494c42bbbc8737af
artemiyblosch <76250967+artemiyblosch@users.noreply.github.com>
2022-05-07 13:38:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-import Lexer from "./Lexer";
-import Parser from "./Parser";
 
-const code =
-    `
-    `
-
-const lexer = new Lexer(code);
-
-lexer.lexAnalysis()
-
-const parser = new Parser(lexer.tokenList);
-
-const rootNode = parser.parseCode()
-
-parser.run(rootNode);
